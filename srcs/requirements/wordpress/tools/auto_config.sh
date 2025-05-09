c4f518e936bee30b273eaaa4aba854ebadc560b3
cadufrederiksen <cadu.riksen@gmail.com>
2025-05-09 14:44:18 +0200

    wp ok

--- srcs/requirements/wordpress/tools/auto_config.sh
+++ srcs/requirements/wordpress/tools/auto_config.sh
@@ -5,28 +5,31 @@ until mysql -h mariadb -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SHOW DATABASES;" 
     echo "Database is not ready. Retrying in 5 seconds..."
     sleep 5
 done
-	echo "Database is ready!"
-
+echo "Database is ready!"
 
 if [ ! -f /var/www/wordpress/wp-config.php ]; then
+    echo "Creating wp-config.php file..."
+    wp config create --allow-root \
+        --dbhost="mariadb:3306" \
+        --dbname="$MYSQL_DATABASE" \
+        --dbuser="$MYSQL_USER" \
+        --dbpass="$MYSQL_PASSWORD" \
+        --path="/var/www/wordpress"
 
-echo "Creating wp-config.php file..."
-echo "$MYSQL_DATABASE"
-	wp config create --allow-root \
-		--dbhost="mariadb:3306" \
-		--dbname="$MYSQL_DATABASE" \
-		--dbuser="$MYSQL_USER" \
-		--dbpass="$MYSQL_PASSWORD" \
-		--path="/var/www/wordpress"
+    echo "Installing WordPress..."
+    wp core install --allow-root \
+        --url="https://carmarqu.42.fr.com" \
+        --title="Inception" \
+        --admin_user="$WP_ADMIN_USER" \
+        --admin_password="$WP_ADMIN_PASSWORD" \
+        --admin_email="$WP_ADMIN_EMAIL" \
+        --path="/var/www/wordpress"
 
-	wp user create "$WP_ADMIN_USER" "$WP_ADMIN_EMAIL" \
-		--role=administrator \
-		--user_pass="$WP_ADMIN_PASSWORD" --allow-root
-
-	wp user create "$WP_USER" "$WP_USER_EMAIL" \
-		--role=subscriber \
-		--user_pass="$WP_USER_PASSWORD" --allow-root
+    echo "Creating WordPress subscriber user..."
+    wp user create "$WP_USER" "$WP_USER_EMAIL" \
+        --role=subscriber \
+        --user_pass="$WP_USER_PASSWORD" --allow-root \
+        --path="/var/www/wordpress"
 fi
 
 exec php-fpm7.3 -F
-echo "Wp created"
